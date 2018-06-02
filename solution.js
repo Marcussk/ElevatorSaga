84912f014073504b984afc5e3f44c5a5321c131d
Marek Beňo <xbenom01@stud.fit.vutbr.cz>
2018-06-02 23:43:46 +0200

    calling idle elevators from floor

--- solution.js
+++ solution.js
@@ -1,6 +1,27 @@
 {
     init: function(elevators, floors) {
+        // elevator indexes
+        ready_elevators = []
+        // numbers of floors that were requested but no elevators were present
+        missed_calls = []
 
+        var log = function() {
+            console.log(Array.prototype.slice.call(arguments));
+        };
+
+        var callElevator = function(floorNum) {
+            if(ready_elevators.length) {
+                var elevator_index = 0;
+                var elevator = elevators[ready_elevators[elevator_index]];
+                elevator.status = "moving";
+                elevator.goToFloor(floorNum);
+                // remove elevator from ready elevators
+                ready_elevators.splice(elevator_index, 1);
+            }
+            else {
+                missed_calls.push(floorNum);
+            }
+        }
 
         // Setup each elevator
         for (var i = 0; i < elevators.length; i++) {
@@ -14,18 +35,27 @@
                     // passenger inside elevator pressed button
                     elevator.on("floor_button_pressed", function(floorNum) {
                         elevator.goToFloor(floorNum);
+                        this.status = "loaded";
                     });
 
                     // Whenever the elevator is idle (has no more queued destinations) ...
                     elevator.on("idle", function() {
                         elevator.goToFloor(0);
-                        //    this.status = "idle";
+                        this.status = "idle";
+                        ready_elevators.push(elevator.id);
                     }); 
                 }
             )(elevators[i]);
         }
+        _.each(floors, function(floor) {
+            floor.on("up_button_pressed", function() {
+                callElevator(floor.floorNum());
+            })
+            floor.on("down_button_pressed", function() {
+                callElevator(floor.floorNum());
+            })
+        })
     },    
-        // _.each(floors, function(floor) {    },
         update: function(dt, elevators, floors) {
             // We normally don't need to do anything here
         }
